fd7154ec3b1044037f04e5ae8367240ed6aeca84
Anurag Goel <thechampanurag@gmail.com>
2017-10-31 09:51:25 +0530

    apples and oranges

---
+++ implementation/apple_and_orange.py
@@ -0,0 +1,13 @@
+import sys
+
+
+s,t = raw_input().strip().split(' ')
+s,t = [int(s),int(t)]
+a,b = raw_input().strip().split(' ')
+a,b = [int(a),int(b)]
+m,n = raw_input().strip().split(' ')
+m,n = [int(m),int(n)]
+apple = map(int,raw_input().strip().split(' '))
+orange = map(int,raw_input().strip().split(' '))
+print len(filter(lambda x:(x>=s and x<=t),map(lambda x:x+a,apple)))
+print len(filter(lambda x:(x>=s and x<=t),map(lambda x:x+b,orange)))
